69522bb748e8754e5d525380d5624c4133c10595
ahmad <kadyroff666@icloud.com>
2020-10-06 22:44:41 +0200

    4

--- src/js/classes/Render.js
+++ src/js/classes/Render.js
@@ -6,6 +6,7 @@ export default class Render {
 
     init() {
         this[this.component]();
+        return this;
     }
 
     app() {
--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,15 @@ import Render from "./classes/Render";
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Render('app');
-    app.init();
-    app.loaderRender();
-    app.feedbackRender();
+    app
+        .init()
+        .loaderRender()
+        .feedbackRender();
     getData()
         .then(result => {
-            app.loaderRemove();
-            app.listRender();
-            app.resultRender(result);
+            app
+                .loaderRemove()
+                .listRender()
+                .resultRender(result);
         });
 })
